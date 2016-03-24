7df7ab3c0876bd5bfef7fc00720d884ae14c63aa
Geordie Millar <gm@stackunderflow.com>
2016-03-24 14:44:20 +1100

    dl script

---
+++ tileserver-2/scripts/04-osm-download.sh
@@ -0,0 +1,8 @@
+cat > /tmp/download.sh << EOF
+	set -euxo pipefail
+	cd ~
+	axel -n 8 http://download.geofabrik.de/australia-oceania/australia-latest.osm.pbf
+EOF
+
+su osm -c "bash /tmp/download.sh"
+read -p "press enter..."
