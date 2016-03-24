2800a6f5ec37eaead5a64fe52705b8368314c7a2
Geordie Millar <gm@stackunderflow.com>
2016-03-24 13:40:11 +1100

    tileserver2 - master initial

---
+++ tileserver-2/master.sh
@@ -0,0 +1,28 @@
+set -euxo pipefail
+
+apt-get -y install dialog
+
+OPTIONS=(	1 "Update Debian and install required utilities"
+			2 "Postgres - apply tuning"
+			3 "Postgres - create database"
+			4 "Download OSM data"
+			5 "Setup internal net for Vultr hosts"
+			6 "Install imposm"
+			7 "Imposm initial read"
+			8 "Imposm import into database"
+			9 "Imposm optimise and deploy")
+
+CHOICE=$(dialog --clear --backtitle "StackPtr Deploy" --menu "Choose a task:" 15 40 4 "${OPTIONS[@]}" 2>&1 >/dev/tty)
+clear
+
+case $CHOICE in
+	1) bash scripts/01-update-debian.sh ;;
+	2) bash scripts/02-postgres-tune.sh ;;
+	3) bash scripts/03-postgres-createdb.sh ;;
+	4) bash scripts/04-osm-download.sh ;;
+	5) bash scripts/05-vultr-net.sh ;;
+	6) bash scripts/06-imposm3-install.sh ;;
+	7) bash scripts/07-imposm3-read.sh ;;
+	8) bash scripts/08-imposm3-import.sh ;;
+	9) bash scripts/09-imposm3-deploy.sh ;;
+esac
