7363aea2fe389af11a124b34493c28af0ab51a0e
Geordie Millar <gm@stackunderflow.com>
2016-03-24 14:25:31 +1100

    postgres tune update

--- tileserver-2/scripts/02-postgres-tune.sh
+++ tileserver-2/scripts/02-postgres-tune.sh
@@ -2,11 +2,11 @@ set -euxo pipefail
 
 PG_CONF_PATH=/etc/postgresql/9.5/main/
 
-sed -i -e"s/^#max_connections = .*$/max_connections = 250/" $PG_CONF_PATH/postgresql.conf
-sed -i -e"s/^#shared_buffers = .*$/shared_buffers = 1GB/" $PG_CONF_PATH/postgresql.conf
-sed -i -e"s/^#work_mem = .*$/work_mem = 512MB/" $PG_CONF_PATH/postgresql.conf
-sed -i -e"s/^#fsync = .*$/fsync = off/" $PG_CONF_PATH/postgresql.conf
-sed -i -e"s/^#synchronous_commit = .*$/synchronous_commit = off/" $PG_CONF_PATH/postgresql.conf
-sed -i -e"s/^#checkpoint_completion_target = .*$/checkpoint_completion_target = 0.9/" $PG_CONF_PATH/postgresql.conf
+sed -r -e"s/^#?max_connections = .*$/max_connections = 250/" $PG_CONF_PATH/postgresql.conf
+sed -r -e"s/^#?shared_buffers = .*$/shared_buffers = 1GB/" $PG_CONF_PATH/postgresql.conf
+sed -r -e"s/^#?work_mem = .*$/work_mem = 512MB/" $PG_CONF_PATH/postgresql.conf
+sed -r -e"s/^#?fsync = .*$/fsync = off/" $PG_CONF_PATH/postgresql.conf
+sed -r -e"s/^#?synchronous_commit = .*$/synchronous_commit = off/" $PG_CONF_PATH/postgresql.conf
+sed -r -e"s/^#?checkpoint_completion_target = .*$/checkpoint_completion_target = 0.9/" $PG_CONF_PATH/postgresql.conf
 
 service postgresql restart
