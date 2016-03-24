9b45c422061a710886f9615f2c87bf1b3e14e6fc
Geordie Millar <gm@stackunderflow.com>
2016-03-24 14:34:12 +1100

    add postgres createdb

---
+++ tileserver-2/scripts/03-postgres-createdb.sh
@@ -0,0 +1,10 @@
+cat > /tmp/postgres.sh << EOF
+	set -euxo pipefail
+	createuser --no-superuser --no-createrole --createdb osm
+	createdb -E UTF8 -O osm osm
+	psql -d osm -c "CREATE EXTENSION postgis;"
+	psql -d osm -c "CREATE EXTENSION hstore;"
+	echo "ALTER USER osm WITH PASSWORD 'osm';" | psql -d osm
+EOF
+
+su postgres -c "bash /tmp/postgres.sh"
