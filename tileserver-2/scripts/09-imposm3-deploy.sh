7eccecb5feb1cb31d4cc1e54403eeeba27f9eac3
Geordie Millar <gm@stackunderflow.com>
2016-03-24 15:34:48 +1100

    remove config.json

--- tileserver-2/scripts/09-imposm3-deploy.sh
+++ tileserver-2/scripts/09-imposm3-deploy.sh
@@ -1,8 +1,8 @@
 cat > /tmp/imposm3-deploy.sh << EOF
 	set -euxo pipefail
 	cd ~
-	~/go/bin/imposm3 import -mapping example-mapping.json -connection postgis://osm:osm@localhost/osm?prefix=planet_osm_ -cachedir imposm-cache -config config.json -optimize
-	~/go/bin/imposm3 import -mapping example-mapping.json -connection postgis://osm:osm@localhost/osm?prefix=planet_osm_ -cachedir imposm-cache -config config.json -deployproduction
+	~/go/bin/imposm3 import -mapping example-mapping.json -connection postgis://osm:osm@localhost/osm?prefix=planet_osm_ -cachedir imposm-cache -optimize
+	~/go/bin/imposm3 import -mapping example-mapping.json -connection postgis://osm:osm@localhost/osm?prefix=planet_osm_ -cachedir imposm-cache -deployproduction
 EOF
 
 su osm -c "bash /tmp/imposm3-deploy.sh" 
