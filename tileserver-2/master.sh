15a656b9b11fe8a4d1bcbbbc90ba5edb88ef6720
Geordie Millar <gm@stackunderflow.com>
2016-03-24 13:48:24 +1100

    tileserver-2: infinite loop

--- tileserver-2/master.sh
+++ tileserver-2/master.sh
@@ -10,19 +10,24 @@ OPTIONS=(	1 "Update Debian and install required utilities"
 			6 "Install imposm"
 			7 "Imposm initial read"
 			8 "Imposm import into database"
-			9 "Imposm optimise and deploy")
+			9 "Imposm optimise and deploy"
+			10 "Exit")
 
-CHOICE=$(dialog --clear --backtitle "StackPtr Deploy" --menu "Choose a task:" 15 40 9 "${OPTIONS[@]}" 2>&1 >/dev/tty)
-clear
+while true
+do
+	CHOICE=$(dialog --clear --backtitle "StackPtr Deploy" --menu "Choose a task:" 24 80 12 "${OPTIONS[@]}" 2>&1 >/dev/tty)
+	clear
 
-case $CHOICE in
-	1) bash scripts/01-update-debian.sh ;;
-	2) bash scripts/02-postgres-tune.sh ;;
-	3) bash scripts/03-postgres-createdb.sh ;;
-	4) bash scripts/04-osm-download.sh ;;
-	5) bash scripts/05-vultr-net.sh ;;
-	6) bash scripts/06-imposm3-install.sh ;;
-	7) bash scripts/07-imposm3-read.sh ;;
-	8) bash scripts/08-imposm3-import.sh ;;
-	9) bash scripts/09-imposm3-deploy.sh ;;
-esac
+	case $CHOICE in
+		1) bash scripts/01-update-debian.sh ;;
+		2) bash scripts/02-postgres-tune.sh ;;
+		3) bash scripts/03-postgres-createdb.sh ;;
+		4) bash scripts/04-osm-download.sh ;;
+		5) bash scripts/05-vultr-net.sh ;;
+		6) bash scripts/06-imposm3-install.sh ;;
+		7) bash scripts/07-imposm3-read.sh ;;
+		8) bash scripts/08-imposm3-import.sh ;;
+		9) bash scripts/09-imposm3-deploy.sh ;;
+		10) exit ;;
+	esac
+done
